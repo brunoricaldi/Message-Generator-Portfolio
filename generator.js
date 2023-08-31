586b9aa02d40aa03d7bba454a593245c1d02ca59
Bruno Brito <brunordbrito@gmail.com>
2023-08-30 21:03:50 -0300

    Create anew function to get the random numbers

--- generator.js
+++ generator.js
@@ -1,33 +1,34 @@
 const object = {
     firstMessage: ['The true meaning of life is ', 'What life is all about is, actually, ', 'Well, ladies and gentlemen, coming to think of it, in life, you have '],
-    secondMessage: ['to have fun with your friends. ', 'to date as many people as you can. ', 'to travel around the world. '],
-    thirdMessage: ['Simple as that.', 'That\'s it', 'End of story.']
+    secondMessage: ['to have fun with your friends. ', 'to meet as many new people as you can. ', 'to travel around the world. '],
+    thirdMessage: ['Simple as that.', 'That\'s it.', 'End of story.']
 };
 
 const art = {
     arts: ['(#^.^#)     Iglybuff, the kawaii person.', '(^._.^)~    Spoink, the smart pig.', '<(o.o )>      John, the illusionist.']
 }
 
+const getRandomNumb = array => Math.floor(Math.random() * array.length);
 
 const generateRandomMessage = () => {
-    let randomNumber1 = Math.floor(Math.random() * object.firstMessage.length);
-    let randomNumber2 = Math.floor(Math.random() * object.secondMessage.length);
-    let randomNumber3 = Math.floor(Math.random() * object.thirdMessage.length);
+    let randomNumber1 = getRandomNumb(object.firstMessage);
+    let randomNumber2 = getRandomNumb(object.secondMessage);
+    let randomNumber3 = getRandomNumb(object.thirdMessage);
 
     let randomMessage = object.firstMessage[randomNumber1] + object.secondMessage[randomNumber2] + object.thirdMessage[randomNumber3];
 
     console.log(randomMessage);
     console.log(' ');
     console.log(' ');
-    console.log(' ');
 }
 
 const generateRandomArt = () => {
-    let randomArt = Math.floor(Math.random() * art.arts.length);
+    let randomNumber = getRandomNumb(art.arts);
+    let randomArt = art.arts[randomNumber];
 
     console.log("Author:");
     console.log(' ');
-    console.log(art.arts[randomArt]);
+    console.log(randomArt);
 }
 
 generateRandomMessage();
